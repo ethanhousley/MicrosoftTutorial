0724f602a4462f313a42f6ecaf9feb80a9ba0e02
ethanhousley <66891702+ethanhousley@users.noreply.github.com>
2021-06-22 15:58:00 -0500

    Add project files.

---
+++ MicrosoftTutorial/Controllers/PeopleController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicrosoftTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace MicrosoftTutorial.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+        private readonly ConnectionDBClass _db;
+
+
+        public PeopleController(ConnectionDBClass db)
+        {
+            _db = db;
+        }
+
+        
+
+        public IActionResult Index()
+        {
+            var personList = _db.PrimaryUser.ToList();
+
+            
+
+            return View(personList);
+        }
+
+
+        
+ 
+
+    }
+}
---
+++ MicrosoftTutorial/Models/ConnectionDBClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MicrosoftTutorial.Models
+{
+    public class ConnectionDBClass:DbContext
+    {
+        public ConnectionDBClass(DbContextOptions<ConnectionDBClass> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> PrimaryUser { get; set; }
+    }
+}
